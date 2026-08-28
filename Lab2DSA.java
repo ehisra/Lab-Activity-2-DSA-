66224a7352ff3d97c62716e6e337a946513c1d36
jellyieee <jillianjoyrodrigo790@gmail.com>
2026-08-28 22:16:51 +0800

    Add files via upload
    pa-double check na lang po

--- Lab2DSA.java
+++ Lab2DSA.java
@@ -11,8 +11,16 @@ public class Lab2DSA {
 
         do{
             menu ();
-            choice = sc.nextInt();
 
+            while (!sc.hasNextInt()){
+                System.out.println("Invalid input! Only enter choices 0 - 5. Press any key to continue...");
+                sc.nextLine();
+                sc.nextLine();
+                menu ();
+            }
+            
+            choice = sc.nextInt();
+            
             if (choice > 5 || choice < 0){
                 System.out.println("Invalid input! Only enter choices 0 - 5. Press any key to continue...");
                 sc.nextLine();
@@ -35,15 +43,37 @@ public class Lab2DSA {
                 }else{
                     size = one(sc);
                     created = true;
-                }   
+                }
+                menu (); 
+                while (!sc.hasNextInt()){
+                    System.out.println("Invalid input! Only enter choices 0 - 5. Press any key to continue...");
+                    sc.nextLine();
+                    sc.nextLine();
                 menu ();
+                }
                 choice = sc.nextInt();
                 break;
             case 2:
-                array = two(array, size);
+                array = two(sc, array, size);
                 menu ();
                 choice = sc.nextInt();
                 break;
+            case 3:
+                three(sc, array, count);
+                menu ();
+                choice = sc.nextInt();
+                break;
+            case 4:
+                four (sc, count, array);
+                menu ();
+                choice = sc.nextInt();
+                break;
+            case 5:
+                five (sc, array);
+                menu ();
+                choice = sc.nextInt();
+                break;
+                
             default:
                 if (choice > 5 || choice < 0){
                     System.out.println("Invalid input! Only enter choices 0 - 5. Press any key to continue...");
@@ -58,7 +88,12 @@ public class Lab2DSA {
                 choice = sc.nextInt();
                 break;
             }
-         } while (choice!= 0);
+            
+            if (choice == 0) {
+                zero ();
+            }
+            
+         } while (choice !=0);
     }
 
     public static void menu (){
@@ -74,11 +109,19 @@ public class Lab2DSA {
         System.out.println("[0] Stop");
         System.out.println(" ");
         System.out.print("Enter Choice: ");
+
     }
 
     public static int one (Scanner sc){
         int size;
         System.out.println("Enter your desired array size. Please pick from 5 - 20.");
+        
+        while (!sc.hasNextInt()){
+                System.out.println();
+                System.out.println("Invalid input! Only integers are accepted.");
+                System.out.print("Enter your desired array size. Please pick from 5 - 20.");
+                sc.next(); 
+        }
 
         size = sc.nextInt();
 
@@ -93,9 +136,7 @@ public class Lab2DSA {
         return size;
     }
 
-    public static int[] two (int[] array, int size){
-        Scanner sc = new Scanner (System.in);
-        
+    public static int[] two (Scanner sc, int[] array, int size){
         if (array == null){
             array = new int [size];
         }
@@ -103,11 +144,18 @@ public class Lab2DSA {
         if (count >= size){
             System.out.println("Array is already full! Press any key to continue...");
             sc.nextLine();
+            sc.nextLine();
             return array;
         }
 
         System.out.println("Enter an element or enter -99 to exit: ");
         while(count <= size){
+            while (!sc.hasNextInt()) {
+                System.out.println("Invalid input! Only integers are accepted.");
+                System.out.print("Enter an element or enter -99 to exit: ");
+                sc.next();
+            }
+            
             int element = sc.nextInt();
 
             if (element == -99){
@@ -135,5 +183,100 @@ public class Lab2DSA {
             }
         }
         return array;
+    }
+
+    static void three (Scanner sc, int array[], int count) {
+        System.out.print ("Enter an integer to search: ");
+        boolean found = false;
+        
+        while (!sc.hasNextInt()) {
+                System.out.println("Invalid input! Only integers are accepted.");
+                System.out.print("Enter an integer to search: ");
+                sc.next();
+        }
+        
+        int searchId = sc.nextInt();   
+        int indexCount = 0;
+
+        for (int j = 0; j < count; j++) {
+                if (array[j] == searchId) {
+                    found = true;
+                    indexCount = j;
+                    break;
+                } 
+            }
+        System.out.println();
+        if (found) {
+            System.out.println("Integer " + searchId + " found at index " + indexCount);
+        } else {
+            System.out.println("Integer " + searchId + " not found");
+        }
+        System.out.print("Press Any Key to Continue..."); 
+        sc.nextLine();
+        sc.nextLine();
+    }
+
+    static void four (Scanner sc, int count, int[] array) {
+        System.out.println("Elements of the Array: ");
+        System.out.println();
+        if (count == 0) {
+            System.out.println("Your array is empty!");
+        
+        } else {
+            
+            for (int i = 0; i < count; i += 5) {
+                for (int j = 0; j < 5 && i + j < count; j++) {
+                    System.out.printf("%7d", array[i + j]);
+                } 
+                System.out.println(); 
+            }
+        }
+        System.out.println();
+        //System.out.println();
+        System.out.print("Press Any Key to Continue..."); 
+        sc.nextLine();
+        sc.nextLine();
+    }
+    
+    static void five (Scanner sc, int[] array) {
+        if (count == 0) {
+            System.out.println("Array is empty! Nothing to delete.");
+            System.out.print("Press Any Key to continue");
+            sc.nextLine();
+            sc.nextLine();
+            return;
+        }
+        System.out.print("Enter an integer to delete: ");
+        while (!sc.hasNextInt()) {
+            System.out.println("Invalid input! Only integers are accepted.");
+            System.out.print("Enter an integer to delete: ");
+            sc.next();
+        }
+        int target = sc.nextInt();
+        int targetIndex = -1;
+        
+        for (int i = 0; i < count; i++) {
+            if (array[i] == target) {
+                targetIndex = i;
+                break;
+            }
+        }
+        
+        if (targetIndex == -1) {
+            System.out.println(target + " was not found in the array.");
+        } else {
+            for (int i = targetIndex; i < count -1; i++) {
+                array [i] = array [i + 1];
+            }
+            count --;
+            System.out.println(target + " was deleted");
+        }
+        System.out.print("Press Any Key to continue...");
+        sc.nextLine();
+        sc.nextLine();
+    }
+    
+    static void zero() {
+        System.out.println("Program terminated. Goodbye!");
     }
 }
